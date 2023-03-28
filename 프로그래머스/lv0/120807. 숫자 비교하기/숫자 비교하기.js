136a8991eb2dd0c84f51db2967dba5bff68d5fa5
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-03-28 22:30:16 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    if(num1 == num2){
+        let answer =1;
+        return answer; 
+    }
+    else{
+          let answer =-1;
+        return answer; 
+    }
+}
