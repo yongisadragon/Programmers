e83beaa6d95e603684325b6823f370f94d1dda60
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-04-02 18:03:45 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -1,8 +1,5 @@
 
 function solution(num1, num2) {
-    if(-50000<=num1 && num1<=50000){
-        if(-50000<=num2 && num2<=50000){
-            return num1 + num2;
-        }
-    }
+     const answer = num1 + num2;
+            return answer;
 }
