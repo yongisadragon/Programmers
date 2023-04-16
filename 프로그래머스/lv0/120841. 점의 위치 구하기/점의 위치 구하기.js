d881e9e5edc59295aa0af940f940ea26a0b20aa6
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-04-16 19:01:42 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,4 +1,4 @@
 function solution(dot) {
-    const result = dot[0]>0 && dot[1]>0 ? 1: dot[0]<0 && dot[1]>0 ? 2 : dot[0]<0 && dot[1]<0 ? 3 : 4;
-    return result;
+    return dot[0]>0 && dot[1]>0 ? 1: dot[0]<0 && dot[1]>0 ? 2 : dot[0]<0 && dot[1]<0 ? 3 : 4;
+     
 }
