459ad714876f75f2501c9c9cc3769648dbb88522
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-04-19 23:34:50 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,4 +1,6 @@
 function solution(my_str, n) {
-    const reg = new RegExp(`.{1,${n}}`,'g');
-    return my_str.match(reg);
+  let result = []
+  for (let i = 0; i < my_str.length; i += n) //i는 0부터 시작해서 그 다음은 n=6이라고 그 다음 i=6 아래 slice는 slice(6,12)
+  result.push(my_str.slice(i, i + n)); //인덱스는 첫번째가 0이니까 i+n을 해주면 n=6이라고 해도 6개씩 잘린다.
+  return result;
 }
