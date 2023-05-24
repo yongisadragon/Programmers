647f76daedaa51e52e0b96a69966f71b5cc5c2ee
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-05-24 14:55:50 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,8 +1,4 @@
 function solution(my_string) {
     const result = [];
-    const arr2 = my_string.split('');
-    for(let i = arr2.length; i>=0; i--){
-        result.push(arr2[i])
-    }
-    return result.join('');
+    return my_string.split('').reverse().join('')
 }
