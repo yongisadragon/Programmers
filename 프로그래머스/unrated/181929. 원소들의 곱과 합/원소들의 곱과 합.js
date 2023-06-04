33a8b1e48d3a0ff73b7811d3265fcab3cf00b1b3
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-06-04 22:15:56 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,13 +1,9 @@
 function solution(num_list) {
     let sum = 0;
     let multi =1;
-   for(let i=0; i<num_list.length; i++){
-       sum += num_list[i]
-       multi *= num_list[i]
+   for(const i of num_list) {
+       sum += i
+       multi *= i
    }
-    if(multi<sum**2){
-        return 1
-    } else {
-        return 0
-    }
+   return multi < sum*sum ? 1:0;
 }
