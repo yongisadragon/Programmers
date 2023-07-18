ba554e6c5f8c6eff88b80d657971b5023ffa648c
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-07-18 23:10:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,5 +1,7 @@
 function solution(my_string) {
-   return my_string.split('').map(el=>
-         el.match(/[a-z]/g)? el.toUpperCase():el.toLowerCase() )
-  .join('')
+   let str =  my_string.split('').map(el=>{
+       if(el === el.toUpperCase()) {return el.toLowerCase()}
+else {return el.toUpperCase()}
+   })
+  return str.join('')
 }
