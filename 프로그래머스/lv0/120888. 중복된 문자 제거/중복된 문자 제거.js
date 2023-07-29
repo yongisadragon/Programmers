6449c48ee274a082c18ae6da529c963e009cb94d
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-07-29 23:33:44 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    let answer = '';
+    for(let i=0; i<my_string.length;i++){
+        if(my_string.indexOf(my_string[i])===i) {answer+=my_string[i]} 
+    } 
+    return answer;
+}
