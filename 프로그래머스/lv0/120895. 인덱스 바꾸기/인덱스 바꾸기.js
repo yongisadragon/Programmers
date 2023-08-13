e88159c7f63736344edeb2362f025419938b6f94
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-08-14 00:44:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+ 
+    let arr = [...my_string]
+    let temp = arr[num1]
+    arr[num1] = arr[num2]
+    arr[num2] = temp
+    
+    return arr.join('')
+}
