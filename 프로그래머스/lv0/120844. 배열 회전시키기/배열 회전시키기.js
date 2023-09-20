8173f9f02d302346adb38ca99af88b06c60f63db
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-09-20 22:35:58 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,12 +1,4 @@
 function solution(numbers, direction) {
-    let leng = numbers.length -1
-   if(direction==='right'){
-       numbers.unshift(numbers[leng])
-        numbers.pop()
-       return numbers
-   } else {
-       numbers.push(numbers[0])
-       numbers.shift()
-       return numbers
-   }
+     direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift())
+    return numbers
 }
