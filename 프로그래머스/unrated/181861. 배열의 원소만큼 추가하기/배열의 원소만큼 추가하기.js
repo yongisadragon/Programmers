8dca1c9215c10f4a3979248e04e97112e5bbb979
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-10-04 00:02:35 +0900

    [unrated] Title: 배열의 원소만큼 추가하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -1,9 +1,1 @@
-function solution(arr) {
-    var answer = [];
-    for(let i =0; i < arr.length; i++){
-        for(let j=0; j < arr[i]; j++){
-            answer.push(arr[i]);
-        }
-    }
-    return answer;
-}
+const solution = (arr) => arr.reduce((a,c)=>[...a,...Array(c).fill(c)],[])
