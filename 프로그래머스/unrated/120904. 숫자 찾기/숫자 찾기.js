19c59e8c4932b3de2f645e5712e86f5215e64438
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-10-23 23:58:28 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,4 @@
 function solution(num, k) {
-    let arr = num.toString().split('')
-     let answer = arr.findIndex((a)=>a==k.toString())
-     return answer<0 ?  -1 : answer+1
+    //indexOf() 는 일치하는 값의 인덱스를 반환. 없으면 -1. findIndex(()=>{})는 콜백함수형태로 검사를 해줌. {}에는 조건이옴. 반환해주는 값은 일치하는 인덱스 혹은 -1로 indexOf()와 동일.
+    return num.toString().split('').map((el)=>Number(el)).indexOf(k) + 1 || -1
 }
