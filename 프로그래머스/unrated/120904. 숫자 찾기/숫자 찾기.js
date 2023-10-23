0bd74949ad8a54677018ee9ef27daec3ec7649ee
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-10-23 23:54:30 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,5 @@
 function solution(num, k) {
     let arr = num.toString().split('')
      let answer = arr.findIndex((a)=>a==k.toString())
-     return answer !== -1 ? answer+1 : -1
+     return answer<0 ?  -1 : answer+1
 }
