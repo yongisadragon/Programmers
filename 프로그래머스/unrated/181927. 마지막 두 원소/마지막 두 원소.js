9958b3708510e970bd7b42ef2dd4d53dc0e00a1e
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-11-25 23:06:06 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
-    let last = num_list[num_list.length-1]
-    let beforeLast = num_list[num_list.length-2]
-    let answer = [...num_list]
-    last > beforeLast? answer.push(last-beforeLast) : answer.push(last*2)
-    return answer;
+    const [last,before] = [...num_list].reverse()
+    return [...num_list,last>before?last-before:last*2]
 }
