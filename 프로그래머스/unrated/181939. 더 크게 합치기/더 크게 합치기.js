e2c75318fdf78831ff75b317baa45e6363b48dc2
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-11-28 23:54:18 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,4 @@
 function solution(a, b) {
-    let aa = (''+a+b)*1
-    let bb = (''+b+a)*1
-    return aa >= bb ? aa : bb
+   const cal = (a,b) => `${a}${b}`
+   return Math.max(cal(a,b),cal(b,a))
 }
