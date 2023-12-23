768c8829fb161b2cfbf48af9ab137376089c773a
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2023-12-23 22:58:26 +0900

    [level unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,5 +1,5 @@
 function solution(str_list, ex) {
     
-    return str_list.filter(e=>!e.includes(ex)).join('')
-    
+    //return str_list.filter(e=>!e.includes(ex)).join('')
+    return str_list.reduce((acc,cur)=> acc + (cur.includes(ex) ? '' : cur) ,'')
 }
