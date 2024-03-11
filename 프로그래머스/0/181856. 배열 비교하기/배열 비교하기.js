1407f52d319eee07b05c078cc77787261ddf702e
Lee Sang Yong <122862593+yongisadragon@users.noreply.github.com>
2024-03-12 00:17:23 +0900

    [level 0] Title: 배열 비교하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,7 +1,13 @@
 function solution(arr1, arr2) {
-    if(arr1.length > arr2.length) return 1
-    else if(arr1.length<arr2.length) return -1
-    else if(arr1.length=arr2.length) {
-       return arr1.reduce((a,c)=>a+c,0) > arr2.reduce((a,c)=>a+c,0) ? 1 : arr1.reduce((a,c)=>a+c,0) < arr2.reduce((a,c)=>a+c,0) ? -1 : 0
+    let answer = 0;
+    let leng1 = arr1.length
+    let leng2 = arr2.length
+    let add1 = arr1.reduce((a,c)=>a+c,0)
+    let add2 = arr2.reduce((a,c)=>a+c,0)
+    if (leng1 !== leng2) {
+        answer = leng1 > leng2 ? 1 : -1;
+    }else {
+       answer = add1 > add2 ? 1 : add1 < add2 ? -1 : 0;
     }
+    return answer;
 }
